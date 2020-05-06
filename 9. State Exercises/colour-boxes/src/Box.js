3b4af837aead21c7ede0de6a42f8bf034fde702e
MitchAhoy <51808358+MitchAhoy@users.noreply.github.com>
2020-05-06 17:40:12 +1000

    remvoe junk from colour boxes

--- 9. State Exercises/colour-boxes/src/Box.js
+++ 9. State Exercises/colour-boxes/src/Box.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react'
 import './Box.css'
 
 class Box extends Component {
-    constructor(props) {
-        super(props)
-        this.state = { currColour: this.props.colour }
-    }
     render() {
         return (
             <div className="Box" onClick={this.props.onClick} style={{ backgroundColor: this.props.colour }}></div>
