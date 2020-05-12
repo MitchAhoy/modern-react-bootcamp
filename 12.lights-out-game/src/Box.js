c3e685319f3f889c5d05dbfd3b2c73505beb4430
MitchAhoy <51808358+MitchAhoy@users.noreply.github.com>
2020-05-12 22:52:49 +1000

    work on lights out logic + start react forms

--- 12.lights-out-game/src/Box.js
+++ 12.lights-out-game/src/Box.js
@@ -3,7 +3,7 @@ import './Box.css'
 
 
 const Box = (props) => {
-        return <div onClick={props.handleIsLit} className={`Box ${props.isLit ? 'Box-light' : 'Box-dark'}`}></div>
+        return <div onClick={props.onClick} className={`Box ${props.isLit ? 'Box-light' : 'Box-dark'}`}></div>
 }
 
 export default Box
--- 12.lights-out-game/src/GameBoard.js
+++ 12.lights-out-game/src/GameBoard.js
@@ -7,18 +7,30 @@ function GameBoard() {
 
     const randLit = () => (Math.floor(Math.random() * 2)) === 1
     const [gameBoard, setGameBoard] = useState([...new Array(25)].map(box => randLit()))
+    const [isWinner, setIsWinner] = useState(false)
 
-    const handleIsLit = (evt) => {
-        console.log('working')
+    const handleIsLit = (idx, box) => {
+        const newBoard = [...gameBoard]
+        newBoard[idx] = !box
+
+        const handleRightEdges = idx - 1 !== 4 && idx - 1 !== 9 && idx - 1 !== 14 && idx - 1 !== 19
+        const handleLeftEdges = idx + 1 !== 5 && idx + 1 !== 10 && idx + 1 !== 15 && idx + 1 !== 20
+
+        if (newBoard[idx - 1] !== undefined && handleRightEdges) newBoard[idx - 1] = !newBoard[idx - 1]
+        if (newBoard[idx + 1] !== undefined && handleLeftEdges) newBoard[idx + 1] = !newBoard[idx + 1]
+        if (newBoard[idx - 5] !== undefined) newBoard[idx - 5] = !newBoard[idx - 5]
+        if (newBoard[idx + 5] !== undefined) newBoard[idx + 5] = !newBoard[idx + 5]
+
+        setGameBoard(newBoard)
+
+        newBoard.every(box => box) && setIsWinner(true)
+
+        console.log(isWinner)
     }
-
-
-
-
 
     return (
         <div className="GameBoard">
-            {gameBoard.map((box, idx) => <Box isLit={box} key={uuidv4()} onClick={handleIsLit} />)}
+            {gameBoard.map((box, idx) => <Box isLit={box} key={uuidv4()} onClick={() => handleIsLit(idx, box)} idx={idx} />)}
         </div>
     )
 
