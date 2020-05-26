829c4bc7a3e295fdd0da9e173c4964010ef414de
MitchAhoy <51808358+MitchAhoy@users.noreply.github.com>
2020-05-26 20:11:48 +1000

    lifecycle methods

---
+++ 16. Lifecycle Methods/lifecycle-methods/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ZenQuote from './ZenQuote'
+import GithubUserInfo from './GithubUserInfo'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <GithubUserInfo username='facebook' />
+    </div>
+  );
+}
+
+export default App;
